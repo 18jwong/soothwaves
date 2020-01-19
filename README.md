96a253ef85267b1780d7c3dd1e8afdd343bb22b0
18jwong <18jwong@shcp.edu>
2020-01-19 04:14:30 -0800

    added bass

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # soothwaves
+
+## Getting Started
+
+My project makes use of midiutil and will need to be installed using the following command.
+
+pip install midiutil
+
+## Usage
+
+soothwaves.py <output_file_name> <number_of_notes>
--- soothwaves.py
+++ soothwaves.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import math
 from midiutil import MIDIFile
 
 midiName = sys.argv[1]
@@ -11,10 +12,10 @@ track    = 0
 channel  = 0
 time     = 0    # In beats
 duration = 1    # In beats
-tempo    = 120   # In BPM
+tempo    = 240   # In BPM
 volume   = 100  # 0-127, as per the MIDI standard
 
-MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
+MyMIDI = MIDIFile(2)  # One track, defaults to format 1 (tempo track is created
                       # automatically)
 MyMIDI.addTempo(track, time, tempo)
 
@@ -23,6 +24,20 @@ for i in range(numNotes):
 	pitch = degrees[n]
 	MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
 
+numChords = math.floor(numNotes/4)
+degrees  = [48, 50, 52, 55, 57, 60] 
+track    = 1
+channel  = 0
+time     = 0    # In beats
+duration = 4    # In beats
+for i in range(numChords):
+	n = random.randint(0,len(degrees)-1)
+	pitch = degrees[n]
+	MyMIDI.addNote(track, channel, pitch, time + i*4, duration, volume)
+
 # ex name: "major-scale.mid"
-with open(midiName, "wb") as output_file:
-    MyMIDI.writeFile(output_file)
+try:
+	output_file = open(midiName, "wb")
+	MyMIDI.writeFile(output_file)
+except:
+	print("soothwaves.py error: make sure the midi file isn't open")
